93ac4916ae7c29b10babe5a7b1424ff75c0dc694
idli69 <idlidev69@gmail.com>
2026-06-08 11:16:18 +0530

    Fixed gh-cli and git issues

--- home/git.nix
+++ home/git.nix
@@ -3,10 +3,14 @@
   programs.git = {
     enable = true;
     settings = {
-      user.name        = "idli69";
-      user.email       = "idlidev69@gmail.com";
+      user.name = "idli69";
+      user.email = "idlidev69@gmail.com";
       init.defaultBranch = "main";
-      pull.rebase        = true;
+      pull.rebase = true;
     };
+  };
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper.enable = true;
   };
 }
